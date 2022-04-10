c96fcbdd0d407db5554c8de01201730caef015a6
Arndt <arndt_simon@web.de>
2022-04-10 22:13:28 +0200

    Add test call for DataFactory functions as komments

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print('\nHello world')
+"""from src import * 
+
+def main():
+    test = DataFactory.loadInputVideoFromPath("./data/20220301-1638-214.mp4")
+    test = DataFactory.loadInputCsvFromPath("./data/2022-03-01_17-38_positions.csv")
+
+if __name__ == "__main__":
+    main()
+"""
