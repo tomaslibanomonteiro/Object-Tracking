17a4856ede627f29aad30f8889f4391c90292552
tiago-falves <tiago.falves98@gmail.com>
2022-04-22 08:56:05 +0100

    Fixed Docker and added Loadcsv

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ WORKDIR  /src
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 ADD main.py .
-CMD ["python", "-u", "train.py"]
+CMD ["python", "-u", "train.py", "--videos", "data/videos/20220301-1638-214.mp4", "--datafiles", "data/datafiles/2022-03-01_17-38_positions.csv"]
 
 # python train.py --videos data/videos/20220301-1638-214.mp4 --datafiles data/datafiles/2022-03-01_17-38_positions.csv
--- utils/DataFactory.py
+++ utils/DataFactory.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 
-import cv2 
+# import cv2
 
 
-## @brief imports mp4 from path for further processing
+# @brief imports mp4 from path for further processing
 # @param path to mp4 file
 # @return video as cv2VideoCapture - None on error
 def loadInputVideoFromPath(path):
@@ -14,21 +14,40 @@ def loadInputVideoFromPath(path):
         return None
     else:
         fps = int(vid_input.get(5))
-        print("Frame Rate : ",fps,"frames per second") 
-        
+        print("Frame Rate : ", fps, "frames per second")
+
         frame_count = vid_input.get(7)
         print("Frame count : ", frame_count)
     return vid_input
 
-## @brief import csv from path for further processing
+# @brief import csv from path for further processing
 # @param path to csv file
 # @param fields to be importet from csv, default:['ts in ms', 'mapped id', 'x in m', 'y in m', 'direction of movement in deg']
-# @return panda df 
+# @return panda df
+
+
+class PersonTime:
+    def __init__(self, time_id, person_id, x_coord, y_coord):
+        self.time_id = time_id
+        self.person_id = person_id
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+
+
 def loadInputCsvFromPath(path, fields=['ts in ms', 'mapped id', 'x in m', 'y in m', 'direction of movement in deg']):
-    df = pd.read_csv(path, sep=';', header=0, skipinitialspace=True, usecols=fields)
-    print(df.columns)
-    print("Rows in csv count : ", len(df))
-    return df
+    df = pd.read_csv(path, sep=';', header=0,
+                     skipinitialspace=True, usecols=fields)
+    people_times = []
+    for row_idx in df.index:
+        person_time = PersonTime(
+            df['ts in ms'][row_idx],
+            df['mapped id'][row_idx],
+            df['x in m'][row_idx],
+            df['y in m'][row_idx])
+        people_times.append(person_time)
+
+    return people_times
+
 
 def videoCaptureToNpArray(cap, start_frame, num_frames=100, out_fps=1):
     frames = []
@@ -36,21 +55,31 @@ def videoCaptureToNpArray(cap, start_frame, num_frames=100, out_fps=1):
     ret = True
     frame_number = start_frame
     end_frame = start_frame + num_frames * int(fps/out_fps)
-    
-    while ret and frame_number<=end_frame:
+
+    while ret and frame_number <= end_frame:
         cap.set(cv2.CAP_PROP_FRAME_COUNT, frame_number-1)
-        ret, frame = cap.read() # read one frame from the 'capture' object; img is (H, W, C)
+        ret, frame = cap.read()  # read one frame from the 'capture' object; img is (H, W, C)
         if ret:
             frames.append(frame)
         frame_number += int(fps/out_fps)
-    video = np.stack(frames, axis=0) # dimensions (T, H, W, C)
+    video = np.stack(frames, axis=0)  # dimensions (T, H, W, C)
     return video
+
 
 def downsampleInput(df_video, df_csv):
     pass
 
+
 def decreaseDataframe(df_csv):
     pass
 
+
 def createSnippet():
     pass
+
+
+print("Hello world")
+ola = loadInputCsvFromPath('../data/datafiles/2022-03-01_17-38_positions.csv')
+# ola = loadInputCsvFromPath('../data/datafiles/2022-0301_17-38_postion_small.csv')
+
+print(ola[0].time_id)
