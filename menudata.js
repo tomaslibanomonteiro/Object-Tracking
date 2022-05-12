4f4c556fe1ecf183e4904d8a38f3b9d9b65874ad
trondvat <trondvat@users.noreply.github.com>
2022-05-12 11:13:13 +0000

    deploy: de87d970c8251deea1e3ea35d187ca31a4448d62

--- menudata.js
+++ menudata.js
@@ -31,4 +31,8 @@ var menudata={children:[
 {text:"Package Functions",url:"namespacemembers.html",children:[
 {text:"All",url:"namespacemembers.html"},
 {text:"Functions",url:"namespacemembers_func.html"},
-{text:"Variables",url:"namespacemembers_vars.html"}]}]}]}
+{text:"Variables",url:"namespacemembers_vars.html"}]}]},
+{text:"Classes",url:"annotated.html",children:[
+{text:"Class List",url:"annotated.html"},
+{text:"Class Index",url:"classes.html"},
+{text:"Class Hierarchy",url:"hierarchy.html"}]}]}
